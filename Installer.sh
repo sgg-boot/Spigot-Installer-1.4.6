96f85d81823a3acc48c7204ea62ce43c7db93d0b
Siter <60112146+sgg-boot@users.noreply.github.com>
2020-01-25 13:19:03 -0300

    Update Installer.sh

--- Installer.sh
+++ Installer.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
- sudo apt install openjdk-8-jdk openjdk-8-jre wget
- sudo apt install default-jdk default-jre wget   
-     
-   sudo pacman -S jdk8-openjdk jre8-openjdk wget
-     
-     sudo archlinux-java set java-8-openjdk
-       
-       rm -rf *
+  sudo apt install openjdk-8-jdk openjdk-8-jre wget
+  sudo apt install default-jre default-jdk wget
+    sudo yum install openjdk-8-jdk openjdk-8-jre wget
+      sudo dnf install openjdk-8-jdk openjdk-8-jre wget
+        sudo pacman -S jdk8-openjdk jre8-openjdk
+          sudo archlinux-java set java-8-openjdk
+          rm -rf *
            
            wget -O spigot.jar https://cdn.getbukkit.org/spigot/spigot-1.4.6-R0.4-SNAPSHOT.jar
              
